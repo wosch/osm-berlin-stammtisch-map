1aab38b822a85e238de751e77343bbb87774d72b
Wolfram Schneider <wosch@FreeBSD.org>
2024-02-09 17:37:38 +0100

    alot of coding

--- bin/geojsonp.pl
+++ bin/geojsonp.pl
@@ -45,7 +45,9 @@ sub parse_csv {
         }
     );
 
-    open my $fh, "<:encoding(utf8)", $file or die "$file: $!";
+    #open my $fh, "<:encoding(utf8)", $file or die "$file: $!";
+    open my $fh, "<", $file or die "$file: $!";
+    binmode( $fh, ":raw" );
 
     my @rows = ();
     while ( my $row = $csv->getline($fh) ) {
@@ -58,9 +60,79 @@ sub parse_csv {
     return \@rows;
 }
 
+sub name_norm {
+    my $name = shift;
+
+    my $x = $name;
+    $name = lc($name);
+    $name =~ s/\s*[,\(]\s*.*//;
+    $name =~ s/\s+\-.*//;
+
+    return $name;
+}
+
+sub location_hash {
+    my $l = shift;
+
+    die "missing list\n" if ref $l ne 'ARRAY';
+
+    my @location = @$l;
+    my $hash;
+
+    foreach my $loc (@location) {
+        my ( $lon, $lat, $name, $street, $city ) = @$loc;
+        my $name_norm = name_norm($name);
+
+        $hash->{$name_norm} = {
+            'lon'       => $lon,
+            'lat'       => $lat,
+            'name'      => $name,
+            'street'    => $street // '',
+            'city'      => $city   // '',
+            'name_norm' => $name_norm
+        };
+        warn "$lon, $lat, $name, $street, $city, $name_norm\n" if $debug >= 2;
+    }
+
+    return $hash;
+}
+
+sub geojsonp {
+    my $location   = shift;
+    my $stammtisch = shift;
+
+    my $hash;
+    foreach my $s (@$stammtisch) {
+        my ( $number, $date, $name, $wiki_count, $real_count ) = @$s;
+        my $name_norm = name_norm($name);
+        $real_count = 'N/A' if $real_count eq '';
+
+        #warn "$number, $date, $name, $wiki_count, $real_count :: $name_norm\n";
+
+        if ( !exists $hash->{$name_norm} ) {
+            if ( !exists $location->{$name_norm} ) {
+                warn "xxx: name=$name name_norm=$name_norm\n";
+            }
+
+#$hash->{ $name_norm } = "bla"; #"<b>" . $location->{$name_norm}->{'name'} . "</b> : "; # .
+#$location->{$name_norm}->{'street'} . " : " .  $location->{$name_norm}->{'city'} . "<br> " .
+#$number . ". " . $date . " : " . $real_count . "<br>";
+        }
+        else {
+            $hash->{$name_norm} .=
+              $number . ". " . $date . " : " . $real_count . "<br>";
+        }
+    }
+
+    return $hash;
+}
+
 #############################################################################
 # main
 #
+binmode( \*STDOUT, ":raw" );
+binmode( \*STDERR, ":raw" );
+
 GetOptions(
     "debug=i" => \$debug,
     "help"    => \$help,
@@ -68,11 +140,17 @@ GetOptions(
 
 &usage if $help;
 
-my @location   = &parse_csv($location_csv);
-my @stammtisch = &parse_csv($stammtisch_csv);
+my $l          = &parse_csv($location_csv);
+my $location   = &location_hash($l);
+my $stammtisch = &parse_csv($stammtisch_csv);
 
-print Dumper( \@location );
-print Dumper( \@stammtisch );
+print Dumper($location);
+my $result = &geojsonp( $location, $stammtisch );
+
+#print Dumper($result);
+
+#print Dumper( \@stammtisch );
+#print Dumper( $location );
 
 # EOF
 
